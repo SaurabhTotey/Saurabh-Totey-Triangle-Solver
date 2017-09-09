f549efb01ff984490caf7722649eb2f9a55890be
Saurabh Totey <SaurabhTotey@gmail.com>
2017-09-09 08:20:18 -0600

    Started making angle conversion boxes that sometimes work

--- src/Graphics.kt
+++ src/Graphics.kt
@@ -38,13 +38,14 @@ class MainWindow{
      */
     init{
         SwingUtilities.invokeAndWait{
-            //Makes the frame with the title and the icon
+            /*
+             * FRAME
+             * Makes the frame with the title and the icon
+             */
             frame = JFrame("Saurabh Totey Triangle Solver")
             frame.iconImage = ImageIO.read(File("res/Icon.png"))
-            frame.layout = MigLayout()
-
+            frame.layout = MigLayout("")
             frame.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
-
             //Makes the frame bounds such that the frame is exactly in the middle of the screen filling half of its width/height
             val allDevices = GraphicsEnvironment.getLocalGraphicsEnvironment().screenDevices
             val topLeftX = allDevices[0].defaultConfiguration.bounds.x
@@ -52,38 +53,61 @@ class MainWindow{
             val screenX = allDevices[0].defaultConfiguration.bounds.width
             val screenY = allDevices[0].defaultConfiguration.bounds.height
             frame.setBounds(screenX / 4 + topLeftX, screenY / 4 + topLeftY, screenX / 2, screenY / 2)
-
             //Makes the font based off of screen size
             titleFont = Font(Font.SERIF, Font.ROMAN_BASELINE, screenX / 70)
             defaultFont = Font(Font.MONOSPACED, Font.PLAIN, screenX / 130)
 
-            //Makes the angles pane which handles angle mode and conversions of angles
+            /*
+             * ANGLE PANE
+             * Makes the angles pane which handles angle mode and conversions of angles
+             */
             var anglesPane = JPanel(MigLayout(""))
             //Below makes the mode which selects whether the triangle inputs should be inputted/outputted as radians or degrees
             val angleOptions = arrayOf("Radians (Rad)", "Degrees (°)")
             var modeDropDownMenu = JComboBox(angleOptions)
             modeDropDownMenu.font = defaultFont
-            modeDropDownMenu.addActionListener{actionEvent ->
-                this.isInRads = (actionEvent.source as JComboBox<*>).selectedItem as String == angleOptions[0]
-            }
+            modeDropDownMenu.addActionListener{actionEvent -> this.isInRads = (actionEvent.source as JComboBox<*>).selectedItem as String == angleOptions[0]}
             anglesPane.add(modeDropDownMenu)
             //Below makes the conversion boxes which will allow users to either enter in a radian value or a degrees value, and the other value will get updated to be equivalent
-            var degreesConversionBox = JTextField()
-            var radiansConversionBox = JTextField()
+            val boxLength = 4
+            var degreesConversionBox = JTextField(boxLength)
+            var radiansConversionBox = JTextField(boxLength)
             degreesConversionBox.addKeyListener(object : KeyListener{
-
-                override fun keyPressed(p0: KeyEvent?) {} override fun keyReleased(p0: KeyEvent?) {}
-
-                override fun keyTyped(p0: KeyEvent?) {}
-
+                override fun keyTyped(p0: KeyEvent?) {} override fun keyReleased(p0: KeyEvent?) {}
+                override fun keyPressed(p0: KeyEvent?) {
+                    radiansConversionBox.text = try{
+                        (mathEngine.evaluate(degreesConversionBox.text) * Math.PI / 180).toString()
+                    }catch(e: Exception){
+                        ""
+                    }
+                    radiansConversionBox.caretPosition = 0
+                }
             })
             radiansConversionBox.addKeyListener(object : KeyListener{
-
-                override fun keyPressed(p0: KeyEvent?) {} override fun keyReleased(p0: KeyEvent?) {}
-
-                override fun keyTyped(p0: KeyEvent?) {}
-
+                override fun keyTyped(p0: KeyEvent?) {} override fun keyReleased(p0: KeyEvent?) {}
+                override fun keyPressed(p0: KeyEvent?) {
+                    degreesConversionBox.text = try{
+                        (mathEngine.evaluate(radiansConversionBox.text) * 180 / Math.PI).toString()
+                    }catch(e: Exception){
+                        ""
+                    }
+                    degreesConversionBox.caretPosition = 0
+                }
             })
+            degreesConversionBox.font = defaultFont
+            radiansConversionBox.font = defaultFont
+            var degreesLabel = JLabel("°")
+            var radiansLabel = JLabel("Rad")
+            var equalsLabel = JLabel(" = ")
+            degreesLabel.font = defaultFont
+            radiansLabel.font = defaultFont
+            equalsLabel.font = defaultFont
+            anglesPane.add(JLabel("    "), "")
+            anglesPane.add(degreesConversionBox, "")
+            anglesPane.add(degreesLabel, "")
+            anglesPane.add(equalsLabel, "")
+            anglesPane.add(radiansConversionBox, "")
+            anglesPane.add(radiansLabel, "")
             frame.add(anglesPane, "dock south")
 
             frame.isVisible = true
