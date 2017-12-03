7df99e15b43df91419e4fd324387e764163f0947
Saurabh Totey <SaurabhTotey@gmail.com>
2017-12-02 20:48:34 -0700

    Added more complete 0 truncation in string formatting

--- src/TriangleDrawer.kt
+++ src/TriangleDrawer.kt
@@ -84,7 +84,8 @@ class TriangleDrawer : JPanel() {
         val truncateLength = 5
         val lineSpacing = 2 * triangleWidth / 35
         fun formatString(toFormat: Double): String {
-            val number: String = toFormat.toString()
+            val numLength = toFormat.toString().length
+            val number: String = toFormat.toString().substring(0..if(numLength - 1 < truncateLength + 1) numLength - 1 else truncateLength + 1)
             return when {
                 number.endsWith("0") && !number.endsWith(".0") -> formatString(number.substring(0 until number.length).toDouble())
                 number.length >= truncateLength + 1 -> {
