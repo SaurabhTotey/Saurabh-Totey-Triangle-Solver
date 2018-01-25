48b08b2e620feeb3d88bf7062e948ea18e56ae9c
Saurabh Totey <SaurabhTotey@gmail.com>
2018-01-24 22:35:59 -0700

    Added more HTML elements

--- src/com/saurabhtotey/trianglesolver/GUI.kt
+++ src/com/saurabhtotey/trianglesolver/GUI.kt
@@ -3,8 +3,11 @@ package com.saurabhtotey.trianglesolver
 import kotlin.browser.document
 import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLDivElement
 
 val screen = document.getElementById("screen") as HTMLCanvasElement
+val ioPane = document.getElementById("ioPane") as HTMLDivElement
+val anglesPane = document.getElementById("anglesPane") as HTMLDivElement
 val renderer = screen.getContext("2d") as CanvasRenderingContext2D
 
 fun main(args: Array<String>) {
