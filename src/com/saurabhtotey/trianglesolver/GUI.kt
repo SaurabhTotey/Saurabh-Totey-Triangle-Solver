d89291572b1a316df2333c5b5636457c4ee3f9d6
Saurabh Totey <SaurabhTotey@gmail.com>
2018-01-24 21:09:20 -0700

    Started making HTML page for Kotlin code

--- src/com/saurabhtotey/trianglesolver/GUI.kt
+++ src/com/saurabhtotey/trianglesolver/GUI.kt
@@ -1,5 +1,12 @@
 package com.saurabhtotey.trianglesolver
 
-fun main(args: Array<String>) {
+import kotlin.browser.document
+import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.CanvasRenderingContext2D
 
+val screen = document.getElementById("screen") as HTMLCanvasElement
+val renderer = screen.getContext("2d") as CanvasRenderingContext2D
+
+fun main(args: Array<String>) {
+    println("Hello World!")
 }
